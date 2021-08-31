f134e810e8a87b4386df7b331b300a778f6a8303
nmevenkamp <niklas.mevenkamp@gmail.com>
2021-09-01 00:32:27 +0200

    remove unused line

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $_SESSION["entities"] = ["projects", "technologies"];
   <link rel="stylesheet" href="styles/filters.css">
   <link rel="stylesheet" href="styles/filter_search.css">
   <script src="https://cdn.jsdelivr.net/npm/less@4.1.1"></script>
-  <!-- <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script> -->
   <script type="text/javascript" src="scripts/filters.js"></script>
   <script type="text/javascript">
     entities = ["projects", "technologies"];
