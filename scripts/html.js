e317aa6e153666e39d467eb63b5ae896f8984ae3
nmevenkamp <niklas.mevenkamp@gmail.com>
2021-09-10 00:08:41 +0200

    WIP: implement EntitiesSearch

--- scripts/html.js
+++ scripts/html.js
@@ -32,46 +32,134 @@ class Div {
         for (let object of this.objects)
             html += object.html;
         this.div.innerHTML = html;
+
+        for (let object of this.objects) {
+            if (typeof object.addFunctions === 'function')
+                object.addFunctions();
+        }
     }
 }
 
-function get_html_attrs_str(html_attrs) {
-    if (html_attrs == null)
+function uuid() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
+
+function getHTMLAttrStr(htmlAttrs) {
+    if (htmlAttrs == null)
         return "";
 
     let res = "";
-    for (const [key, value] of Object.entries(this.html_attrs))
-        res += " " + key + "='" + value + "'";
+    for (const [key, value] of Object.entries(this.htmlAttrs)) {
+        if (value != null)
+            res += " " + key + "='" + value + "'";
+    }
     return res;
 }
 
 class EntitiesTable {
-    constructor(type = null) {
-        this.type = type;
+    constructor(cols=["name"], filterFunc=null) {
+        this.cols = cols;
+        this.filterFunc = filterFunc;
     }
 
     get html() {
-        if (this.type == null)
-            return new Table(entities).html;
-
-        let entities_filtered = [];
-        for (let entity of entities) {
-            if (entity["type"] == this.type) {
-                entities_filtered.push(entity);
+        let entitiesFiltered = [];
+        for (const entity of entities) {
+            if (this.filterFunc == null || this.filterFunc(entity)) {
+                let row = {"entityId": entity["id"], "entityName": entity["name"]};
+                for (const col of this.cols)
+                    row[col] = entity[col];
+                entitiesFiltered.push(row);
             }
         }
-        return new Table(entities_filtered).html;
+        return new Table(entitiesFiltered, ["entityId", "entityName"]).html;
+    }
+}
+
+class EntitiesSearch {
+    constructor(filterFunc=null, placeholder=null, ) {
+        this.filterFunc = filterFunc;
+        this.placeholder = placeholder;
+
+        this.divId = uuid();
+
+        this.activeSet = [];
+    }
+    
+    get html() {
+        let html = "<div" + getHTMLAttrStr({"id": this.divId}) + ">";
+        html += "<input" + getHTMLAttrStr({"data-id": "input", "type": "text", "placeholder": this.placeholder}) + ">";
+        html += "</div>";
+        html += "<div" + getHTMLAttrStr({"data-id": "search-list"}) + ">";
+        html += new EntitiesTable(["name"], this.filterFunc).html;
+        html += "</div>";
+        return html;
+    }
+
+    get div() {
+        return document.getElementById(this.divId);
+    }
+
+    get input() {
+        return this.div.querySelector("[data-id='input']");
+    }
+
+    get searchList() {
+        return this.div.querySelector("[data-id='searchList']");
+    }
+
+    addFunctions() {
+        this.input.ondocus = () => this.show();
+        this.input.onfocusout = () => this.hide();
+        this.input.onkeyup = () => this.update();
+    }
+
+    show() {
+        this.searchList.style.display = "block";
+    }
+
+    hide () {
+        this.searchList.style.display = "none";
+    }
+
+    update () {
+        const inputStr = this.input.value.toUpperCase();
+        let rows = this.searchList.getElementsByTagName("tr");
+        hasEntries = false;
+        matchedEntry = false;
+        for (let row of rows) {
+            entityId = row.getAttribute("data-entityId");
+            if (this.activeSet.includes(entityId))
+                continue;
+            entityName = row.getAttribute("data-entityName");
+            if (entityName.indexOf(inputStr) > -1) {
+                row.style.display = "block";
+                hasEntries = true;
+            } else
+                row.style.display = "none";
+            if (entityName == inputStr)
+            matchedEntry = true;
+        }
+        if (hasEntries)
+            this.show();
+        else
+            this.hide();
+        // button = document.getElementById(entity + ".filter-search.new");
+        // if (filter == "" || matchedEntry)
+        //     button.style.display = "none";
+        // else
+        //     button.style.display = "block";
     }
 }
 
 class Button {
-    constructor(text = "", html_attrs = null) {
+    constructor(text = "", htmlAttrs = null) {
         this.text = text;
-        this.html_attrs = html_attrs;
+        this.htmlAttrs = htmlAttrs;
     }
 
     get html() {
-        return "<button" + get_html_attrs_str(this.html_attrs) + "><p>" + text + "</p><button>";
+        return "<button" + getHTMLAttrStr(this.htmlAttrs) + "><p>" + text + "</p><button>";
     }
 }
 
@@ -92,16 +180,26 @@ class MultiButton {
 }
 
 class Table {
-    constructor(table) {
+    constructor(table, dataAttrKeys=null) {
         this.table = table;
+        this.dataAttrKeys = dataAttrKeys;
     }
 
     get html() {
         let html = "<table>";
         for (let row of this.table) {
-            html += "<tr>";
-            for (let col of Object.values(row))
-                html += "<td>" + col + "</td>";
+            html += "<tr";
+            if (this.dataAttrKeys != null) {
+                for (const [key, col] of Object.entries(row)) {
+                    if (this.dataAttrKeys.includes(key))
+                        html += " data-" + key + "='" + col + "'";
+                }
+            }
+            html += ">";
+            for (const [key, col] of Object.entries(row)) {
+                if (this.dataAttrKeys == null || !this.dataAttrKeys.includes(key))
+                    html += "<td>" + col + "</td>";
+            }
             html += "</tr>";
         }
         html += "</table>";
